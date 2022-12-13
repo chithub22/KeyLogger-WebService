ad96a6306dc09c4fb1d59859d69d49ce555a02d2
Freddox <46601102+0xFreddox@users.noreply.github.com>
2022-12-14 00:35:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ print("if you want use the service Install : ")
 print(":django")
 print(":vsftpd")
 Select_usr = input("Do You Want Install django and vsftpd?:")
-print("[Y]or[N]
+print("[Y]or[N]")
 if Select_usr == 'Y':
  os.system("pip3 install django")
  os.system("sudo apt install vsftpd")
